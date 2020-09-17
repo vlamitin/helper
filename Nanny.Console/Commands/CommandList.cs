408b47b52aaaf74a706f722e03c63083d157d95f
Dmitry Romashov <framebassman@gmail.com>
2020-09-17 10:43:15 +0300

    remove unused

--- Nanny.Console/Commands/CommandList.cs
+++ Nanny.Console/Commands/CommandList.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Nanny.Console.Commands
 {
     public class CommandList : List<Command>
     {
-        private Regex _regex = new Regex("(--[a-z]*)");
         public Command Find(string[] userInput, Command defaultValue)
         {
             if (userInput.Length == 0)
