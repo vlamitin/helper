cc1d509c5fce0a4cd6f3981e897bb0cf24a9c5fe
Dmitry Romashov <framebassman@gmail.com>
2020-09-17 11:21:03 +0300

    add templates

---
+++ Nanny.Console.Tests.Business/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Nanny.Console.Tests.Business
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
